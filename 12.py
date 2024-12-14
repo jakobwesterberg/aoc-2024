797fb7bb9d59e60e418bad5d2a63a35b5082bf20
Jakob Westerberg <westerberg.jakob@gmail.com>
2024-12-14 23:13:54 +0100

    Day 12

--- 12.py
+++ 12.py
@@ -1,83 +1,35 @@
 import argparse
 
-def search(i, j, plant, visited, s):
-    if i < 0 or j < 0 or i >= len(s) or j >= len(s):
-        return (1, 0)
-    elif s[i][j] != plant:
-        return (1, 0)
-    elif (i, j) in visited:
-        return (0, 0)
-    else:
-        visited[(i, j)] = True
-        perimiter = 0
-        area = 1
-        a = search(i - 1, j    , plant, visited, s)
-        perimiter += a[0]
-        area += a[1]
-        a = search(i + 1, j    , plant, visited, s)
-        perimiter += a[0]
-        area += a[1]
-        a = search(i,     j - 1, plant, visited, s)
-        perimiter += a[0]
-        area += a[1]
-        a = search(i,     j + 1, plant, visited, s)
-        perimiter += a[0]
-        area += a[1]
-
-        return (perimiter, area)
-
-def search2(i, j, plant, visited, s, perimiter):
-    if i < 0 or j < 0 or i >= len(s) or j >= len(s):
+def search(i, j, plant, visited, s, perimiter, area):
+    if i < 0 or j < 0 or i >= len(s) or j >= len(s[0]):
         if (i, j) in perimiter:
             perimiter[(i, j)] += 1
         else:
             perimiter[(i, j)] = 1
-        return ([(i, j)], 0)
     elif s[i][j] != plant:
         if (i, j) in perimiter:
             perimiter[(i, j)] += 1
         else:
             perimiter[(i, j)] = 1
-        return ([(i, j)], 0)
     elif (i, j) in visited:
-        return ([], 0)
+        pass
     else:
         visited[(i, j)] = True
-        area = 1
-        a = search2(i - 1, j    , plant, visited, s, perimiter)
-        area += a[1]
-        a = search2(i + 1, j    , plant, visited, s, perimiter)
-        area += a[1]
-        a = search2(i,     j - 1, plant, visited, s, perimiter)
-        area += a[1]
-        a = search2(i,     j + 1, plant, visited, s, perimiter)
-        area += a[1]
+        area[(i, j)] = True
+        search(i - 1, j    , plant, visited, s, perimiter, area)
+        search(i + 1, j    , plant, visited, s, perimiter, area)
+        search(i,     j - 1, plant, visited, s, perimiter, area)
+        search(i,     j + 1, plant, visited, s, perimiter, area)
 
-        return (perimiter, area)
+def diagonal_neighbour(x, y, a):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if ((y + i, x + j) in a) \
+               and ((y + i, x) not in a) \
+               and ((y, x + j) not in a):
+                return True
 
-def asdflkj(x, y, dx, dy, perimiter):
-    if len(perimiter) == 0:
-        return False
-
-    if not (x, y) in perimiter:
-        return False
-
-    del perimiter[(x, y)]
-
-    asdflkj(x + dx, y + dy, dx, dy, perimiter)
-
-    return True
-
-def walk(x, y, dir, sides, perimiter):
-    sides += 1
-    del perimiter[(x, y)]
-
-    if (x + 1, y) in perimiter:
-        sides += walk(x + 1, y, sides, perimiter)
-    elif (x, y + 1) in perimiter:
-        sides += walk(x, y + 1, sides, perimiter)
-    else:
-        return sides
+    return False
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
@@ -91,28 +43,38 @@ if __name__ == '__main__':
     s = s.split('\n')
 
     count = 0
+    count2 = 0
     visited = {}
     for i in range(0, len(s)):
         for j in range(0, len(s[0])):
             perimiter = {}
-            _, area = search2(i, j, s[i][j], visited, s, perimiter)
-            #count += sum(perimiter.values()) * area
-            sides = 0
-            perimiter_x = perimiter.copy()
-            perimiter_y = perimiter.copy()
+            area = {}
+            search(i, j, s[i][j], visited, s, perimiter, area)
+            count += sum(perimiter.values()) * len(area)
 
-            while len(perimiter) != 0:
-                x, y = next(iter(perimiter))
-                walk(x, y, 0, perimiter)
+            a = {}
+            corners = 0
+            for p in area:
+                y, x = p
 
-            if sides != 0:
-                print(s[i][j])
-                print(sides)
-            #while len(perimiter_y) != 0:
-            #    x, y = next(iter(perimiter_y))
-            #    asdflkj(x, y, 0, 1, perimiter_y)
-            #    sides += 1
+                for dy in [-0.5, 0.5]:
+                    for dx in [-0.5, 0.5]:
+                        if (x + dx, y + dy) in a:
+                            a[(x + dx, y + dy)] += 1
+                        else:
+                            a[(x + dx, y + dy)] = 1
 
-            count += sides * area
+                if diagonal_neighbour(x, y, area):
+                    corners += 1
+
+            for k in a:
+                if a[k] == 1:
+                    corners += 1
+                elif a[k] == 3:
+                    corners += 1
+
+            count2 += corners * len(area)
 
     print(count)
+    print(count2)
+
